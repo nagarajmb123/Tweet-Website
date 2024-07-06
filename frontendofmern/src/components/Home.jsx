821e4bc3cf16b216bcb3e0e7a57337e639300cb9
Nagaraj <nagarajmb235@gmail.com>
2024-07-06 15:09:51 +0530

    edited the home page

--- frontendofmern/src/components/Home.jsx
+++ frontendofmern/src/components/Home.jsx
@@ -3,11 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div className="bg-blue-900 text-white min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl mb-11">Welcome to MERN Stack Application</h1>
-        
-      </div>
-    </div>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-6xl mb-20">Welcome to Tweet Website</h1>
+     </div>
+   </div>
   );
 };
 
